13f31214b818e86518d945ee3e881a805ad5afbb
lekan <oyeniranolalekan89@gmail.com>
2024-06-07 13:27:05 +0100

    oo

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,12 +1,32 @@
-import React from 'react'
 
-const Button = ({label, iconURL}) => {
+const Button = ({
+  label,
+  iconURL,
+  backgroundColor,
+  textColor,
+  borderColor,
+  fullWidth,
+}) => {
   return (
-    <button className='flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none rounded-full bg-coral-red text-white border-coral-red'>
-        {label}
-        {iconURL && <img className='ml-2 rounded-full w-5 h-5' src={iconURL} alt="arrow right" />}
-    </button>
-  )
-}
+    <button
+      className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none
+      ${
+        backgroundColor
+          ? `${backgroundColor} ${textColor} ${borderColor}`
+          : "bg-coral-red text-white border-coral-red"
+      } rounded-full ${fullWidth && "w-full"}`}
+    >
+      {label}
 
-export default Button
+      {iconURL && (
+        <img
+          src={iconURL}
+          alt='arrow right icon'
+          className='ml-2 rounded-full bg-white w-5 h-5'
+        />
+      )}
+    </button>
+  );
+};
+
+export default Button;
